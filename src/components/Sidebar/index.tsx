66324b3a890cdda07e0932e35b005bdf5ba8d8b8
Sohee Lim <limsohee1002@gmail.com>
2024-11-13 16:29:13 -0500

    feat: add layout and get sidebar data

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+
+import { useDocsData } from '@docusaurus/plugin-content-docs/client'
+
+const Sidebar: FC = () => {
+  React.useEffect(() => {
+    const main = document.getElementById('new-main')
+    const sidebars = main.getElementsByTagName('aside')
+    Array.from(sidebars).map((sidebar) => {
+      if (sidebar.id !== 'new-sidebar') {
+        sidebar.remove()
+      }
+    })
+  }, [])
+  const visibleItems = useDocsData(undefined)
+  console.log('!!!', visibleItems)
+  return (
+    <aside id="new-sidebar" className="w-sidebar-w">
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+      <div>new sidebar</div>
+    </aside>
+  )
+}
+
+export default Sidebar
